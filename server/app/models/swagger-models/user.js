ff94426d1b478cda0143e6f7d435538a2667f28a
dremonkey <a.deutmeyer@gmail.com>
2014-05-23 20:29:31 -0700

    added additional properties to user swagger model

--- server/app/models/swagger-models/user.js
+++ server/app/models/swagger-models/user.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  id: 'Person',
+  id: 'User',
   properties: {
     id: {
       type: 'string',
@@ -9,15 +9,31 @@ module.exports = {
     },
     firstname: {
       type: 'string',
-      description: 'Firstname of Person'
+      description: 'Firstname of User'
     },
     lastname: {
       type: 'string',
-      description: 'Lastname of Person'
+      description: 'Lastname of User'
     },
     fullname: {
       type: 'string',
-      description: 'Fullname of Person'
+      description: 'Fullname of User'
+    },
+    email: {
+      type: 'string',
+      description: 'Users Email. Set when this user becomes a member. Not available to unauthenticated requests.'
+    },
+    linkedinToken: {
+      type: 'string',
+      description: 'User LinkedIn Token. Set when this user becomes a member. Not available to unauthenticated requests.'
+    },
+    profileImage: {
+      type: 'string',
+      description: 'Image URL'
+    },
+    joined: {
+      type: 'integer',
+      description: 'Unix Time. Set when this user becomes a member.'
     },
     created: {
       type: 'integer',
