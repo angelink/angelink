99938ae22c644b596159cd7e389a7b1bb6f47176
Benjamin Lee <leebyp@gmail.com>
2014-05-31 21:07:28 -0700

    submit button POST skills array to server for insertion into database

--- client/src/app/pages/controllers/profile.ctrl.js
+++ client/src/app/pages/controllers/profile.ctrl.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('n4j.pages.controllers')
-  .controller('ProfileCtrl', function ($scope) {
+  .controller('ProfileCtrl', function ($scope, $http) {
     console.log($scope);
     $scope.user = {
       firstname: 'Peter',
@@ -15,5 +15,13 @@ angular.module('n4j.pages.controllers')
 
     $scope.submitSkills = function(){
       console.log($scope.skills.value);
+      //POST skills to server and connect nodes to the user
+      $http({method: 'POST', url: 'localhost:/3000/user', data: $scope.skills.value}).
+        success(function() {
+          console.log('POST skills success');
+        }).
+        error(function() {
+          console.log('POST skills error');
+        });
     };
   });
