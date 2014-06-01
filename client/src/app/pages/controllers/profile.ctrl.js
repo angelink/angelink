b9928de72b4d8f876e0232dcf5198bbf105557b1
Benjamin Lee <leebyp@gmail.com>
2014-05-31 21:00:04 -0700

    Included input box with tagger functionality in profile

--- client/src/app/pages/controllers/profile.ctrl.js
+++ client/src/app/pages/controllers/profile.ctrl.js
@@ -3,4 +3,17 @@
 angular.module('n4j.pages.controllers')
   .controller('ProfileCtrl', function ($scope) {
     console.log($scope);
+    $scope.user = {
+      firstname: 'Peter',
+      profileImage: 'http://cartwa.com/Puppies_files/PuppyIcon.jpg'
+    };
+
+    $scope.skills = {
+      value: [],
+      options: ['backbonejs', 'angularjs', 'html', 'css', 'javascript']
+    };
+
+    $scope.submitSkills = function(){
+      console.log($scope.skills.value);
+    };
   });
