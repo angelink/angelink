5385ddda377d2682aaea171a2ba727997e929076
Benjamin Lee <leebyp@gmail.com>
2014-06-01 20:13:44 -0700

    Centred divs on pages using flexbox

--- client/src/app/pages/controllers/profile.ctrl.js
+++ client/src/app/pages/controllers/profile.ctrl.js
@@ -3,21 +3,28 @@
 angular.module('n4j.pages.controllers')
   .controller('ProfileCtrl', function ($scope, $http, SkillsFactory) {
     console.log($scope);
+
+    //user details should be from UserService created on login
     $scope.user = {
       firstname: 'Peter',
       profileImage: 'http://cartwa.com/Puppies_files/PuppyIcon.jpg'
     };
 
-    SkillsFactory.get().then(function(skills){
-      $scope.skills = {
-        value: skills,
-        options: ['backbonejs', 'angularjs', 'html', 'css', 'javascript']
-      };
-    });
+    // SkillsFactory.get().then(function(skills){
+    //   $scope.skills = {
+    //     value: [],
+    //     options: skills
+    //   };
+    // });
 
+    $scope.skills = {
+      value: [],
+      options: ['backbonejs', 'angularjs', 'html', 'css', 'javascript', 'codingskillz']
+    };
+
+    //POST user data with skills as property to connect nodes to user
     $scope.submitSkills = function(){
       console.log($scope.skills.value);
-      //POST skills to server and connect nodes to the user
       $http({method: 'POST', url: 'localhost:/3000/user', data: $scope.skills.value}).
         success(function() {
           console.log('POST skills success');
