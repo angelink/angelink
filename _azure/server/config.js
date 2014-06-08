5983b99e6eaa469806ecc3b7f409719217493b62
dremonkey <a.deutmeyer@gmail.com>
2014-06-07 19:07:47 -0700

    changed .gitignore to allow _azure config to be pushed up

---
+++ _azure/server/config.js
@@ -0,0 +1,51 @@
+'use strict';
+
+// ## Configurations
+// Setup your installations for various environments
+
+// Module dependencies
+var path = require('path');
+
+var config = {};
+
+var PORT = process.env.PORT || 3000;
+var SECRET = process.env.COOKIE_SECRET || 'secret';
+var API_VERSION = 'v0';
+
+// ## Production Environment Configurations
+config.production = {
+
+  dirs: {
+    views: path.resolve(__dirname, '../client'),
+    static: path.resolve(__dirname, '../client')
+  },
+  
+  livereload: false,
+  
+  server: {
+    // Host to be passed to node's `net.Server#listen()`
+    host: '127.0.0.1',
+
+    secret: SECRET,
+
+    // Port to be passed to node's `net.Server#listen()`
+    port: PORT,
+
+    // base url
+    // baseUrl: 'http://angelink.azurewebsites.net',
+    baseUrl: 'http://127.0.0.1:' + PORT,
+
+    apiBasePath: '/api/' + API_VERSION
+  },
+
+  // https://developer.linkedin.com/documents/authentication
+  linkedin: {
+    apiKey: process.env.LINKEDIN_APIKEY,
+    secret: process.env.LINKEDIN_SECRET,
+    state: process.env.LINKEDIN_STATE
+  },
+
+  // viewEngine: 'ect'
+};
+
+module.exports = config;
