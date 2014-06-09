9e54d07f3bdb2be9e6868469e4a188fef8063ba6
dremonkey <a.deutmeyer@gmail.com>
2014-06-09 00:07:24 -0700

    fixed problem with chart resizing

--- client/src/app/pages/directives/browse.detail.dir.js
+++ client/src/app/pages/directives/browse.detail.dir.js
@@ -2,15 +2,28 @@
 
 angular.module('n4j.pages.directives')
 
-  .directive('setChartSize', function ($timeout) {
+  .directive('setChartSize', function (_, $window) {
+
+    var ctrl = function ($scope) {
+      this.resizeHandler = function (el) {
+        $scope.chartConfig.size.width = el[0].clientWidth;
+        $scope.$apply();
+      };
+    };
     
-    var link = function (scope, el) {
-      $timeout(function () {
-        scope.chartConfig.size.width = el[0].clientWidth;
-      }, 300);
+    var link = function (scope, el, attrs, ctrl) {
+      
+      setTimeout(function () {
+        ctrl.resizeHandler(el);
+      }, 100);
+
+      angular.element($window).bind('resize', _.debounce(function () {
+        ctrl.resizeHandler(el);
+      }, 100));
     };
 
     return {
+      controller: ctrl,
       link: link
     };
   });
